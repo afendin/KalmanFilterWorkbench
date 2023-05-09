37072915f464e2cc1e1216f5a3727107e94ccafc
Amir Afendin <afendin.amir@gmail.com>
2023-05-09 08:02:43 +0400

    logic fixes

--- src/statemanager.cpp
+++ src/statemanager.cpp
@@ -16,19 +16,18 @@ void StateManager::update(float measured_distance, int time)
     float t = time - m_lastTime;
 
     // get velocity(v) as a derivative of the range(dx / dt);
-    float v = measured_distance - m_measured_distance / t;
-
-    // State Update Equation for velocity
-    float estimated_velocity = m_estimated_velocity +
-                               m_trust_velocity *
-                               ((v - m_estimated_velocity)/t);
-    set_estimated_velocity(estimated_velocity);
+    float v = (measured_distance - m_predicted_distance) / t;
 
     // State Update Equation for position
-    float estimated_distance = m_estimated_distance +
+    float estimated_distance = m_predicted_distance +
                                m_trust_distance *
-                               (measured_distance - m_estimated_distance);
+                               (measured_distance - m_predicted_distance);
     set_estimated_distance(estimated_distance);
+
+    // State Update Equation for velocity
+    float estimated_velocity = m_predicted_velocity +
+                               m_trust_velocity * v;
+    set_estimated_velocity(estimated_velocity);
 
     // State Extrapolation Equation for velocity
     // *here we assume that velocity is constant
